329c111dcd71eec121b7ba8d38b31aa1807f4360
Branden Makana <bmakana@cobalt.com>
2016-01-31 00:26:05 -0800

    - removing packery - switching to isotope, to have sorting - adding sorting by unread status - adding a test newsService with mock data, using it in unit tests - adding an update controller

--- src/test/java/com/pineapplewedding/homepage/domain/NewsServiceTest.java
+++ src/test/java/com/pineapplewedding/homepage/domain/NewsServiceTest.java
@@ -3,7 +3,10 @@ package com.pineapplewedding.homepage.domain;
 import com.pineapplewedding.homepage.HomepageApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -12,7 +15,10 @@ import javax.annotation.Resource;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = HomepageApplication.class)
+@ContextHierarchy( {
+        @ContextConfiguration(name = "parent", classes = HomepageApplication.class),
+        @ContextConfiguration(name = "child", classes = NewsServiceTest.TestConfig.class)
+})
 public class NewsServiceTest {
 
     @Resource
@@ -21,5 +27,13 @@ public class NewsServiceTest {
     @Test
     public void testBuildNewsFeeds() throws Exception {
         newsService.buildNewsFeeds();
+    }
+
+    @Configuration
+    public static class TestConfig {
+        @Bean
+        public NewsService newsService() {
+            return new TestNewsService();
+        }
     }
 }
