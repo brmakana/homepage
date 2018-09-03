b31c705d19ac112c1ba50cbed80c733cd7f143db
Branden Makana <branden.makana@gmail.com>
2018-09-02 22:55:43 -0700

    enabling scheduling

--- src/main/java/io/makana/homepage/HomepageApplication.java
+++ src/main/java/io/makana/homepage/HomepageApplication.java
@@ -1,9 +1,12 @@
 package io.makana.homepage;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HomepageApplication {
 
 	public static void main(String[] args) {
