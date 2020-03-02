cd1acbceef832d1e560bbe5a93e719e9cf7ac95d
Branden Makana <8333518+brmakana@users.noreply.github.com>
2020-03-02 00:19:43 -0800

    Updating to latest spring boot and changing how published dates are determined; most recent article date or published date, whichever is earliest

--- src/main/java/io/makana/homepage/domain/news/NewsFeed.java
+++ src/main/java/io/makana/homepage/domain/news/NewsFeed.java
@@ -5,7 +5,10 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -23,9 +26,10 @@ public class NewsFeed {
     private Date publishedDate;
 
     public NewsFeed(SyndFeed copyFrom, int maxPerSource) {
-        this.setName(copyFrom.getTitle());
-        this.setUrl(copyFrom.getLink());
-        this.setPublishedDate(copyFrom.getPublishedDate());
+        setName(copyFrom.getTitle());
+        setUrl(copyFrom.getLink());
+        setPublishedDate(copyFrom.getPublishedDate());
+
         if (copyFrom.getImage() != null &&
                 copyFrom.getImage().getUrl() != null && !copyFrom.getImage().getUrl().isEmpty()) {
             this.setImageUrl(copyFrom.getImage().getUrl());
@@ -34,14 +38,49 @@ public class NewsFeed {
         if (copyFrom.getEntries() != null && !copyFrom.getEntries().isEmpty()) {
             int feedCount = 0;
             Iterator i = copyFrom.getEntries().iterator();
+            Date mostRecentPublishedDate = null;
             while (i.hasNext() && feedCount <= maxPerSource) {
                 SyndEntry entry = (SyndEntry) i.next();
                 FeedItem feedItem = new FeedItem(entry.getTitle(), entry.getLink());
                 feedItems.add(feedItem);
                 feedCount++;
+                if (entry.getPublishedDate() != null) {
+                    mostRecentPublishedDate = getLatestDate(mostRecentPublishedDate, entry.getPublishedDate());
+                }
+                if (entry.getUpdatedDate() != null) {
+                    mostRecentPublishedDate = getLatestDate(mostRecentPublishedDate, entry.getUpdatedDate());
+                }
             }
+            setPublishedDate(getEarlistDate(mostRecentPublishedDate, getPublishedDate()));
         }
     }
+
+    private Date getLatestDate(final Date first, final Date second) {
+        if (first == null) {
+            return second;
+        } else if (second == null) {
+            return first;
+        }
+        if (first.after(second)) {
+            return first;
+        } else {
+            return second;
+        }
+    }
+
+    private Date getEarlistDate(final Date first, final Date second) {
+        if (first == null) {
+            return second;
+        } else if (second == null) {
+            return second;
+        }
+        if (first.before(second)) {
+            return first;
+        } else {
+            return second;
+        }
+    }
+
     public NewsFeed(SyndFeed copyFrom) {
         this(copyFrom, DEFAULT_MAX_PER_SOURCE);
     }
@@ -49,18 +88,14 @@ public class NewsFeed {
     public boolean isImageSecure() {
         if (imageUrl == null || imageUrl.isEmpty()) {
             return true;
-        } else if (imageUrl.startsWith("https://")) {
-            return true;
-        } else {
-            return false;
-        }
+        } else return imageUrl.startsWith("https://");
     }
 
     public long getEpochPublishTime() {
         return getPublishedDate().getTime();
     }
 
-    private Date getPublishedDate() {
+    public Date getPublishedDate() {
         if (publishedDate != null) {
             return publishedDate;
         }
--- src/test/java/io/makana/homepage/domain/news/NewsFeedTest.java
+++ src/test/java/io/makana/homepage/domain/news/NewsFeedTest.java
@@ -1,19 +1,39 @@
 package io.makana.homepage.domain.news;
 
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class NewsFeedTest {
 
     @Test
-    public void getTimeDifference() {
-        NewsFeed newsFeed = new NewsFeed();
-        Date date = new Date();
-        date.setTime(date.getTime() - Math.round(1000 * 60 * 60));
-//        newsFeed.setPublishedDate(date);
-        System.out.println(newsFeed.getTimeDifference());
+    public void usesLatestDate() {
+        final SyndFeed mockFeed = mock(SyndFeed.class);
+        final Calendar calendar = Calendar.getInstance();
+        final Date earliest, middle, latest;
+        calendar.add(Calendar.YEAR, -1);
+        earliest = calendar.getTime(); // one year ago
+        calendar.add(Calendar.MONTH, 6);
+        middle = calendar.getTime(); // six months ago
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        latest = calendar.getTime(); // now-ish
+        when(mockFeed.getPublishedDate()).thenReturn(latest);
+
+        final SyndEntry entry1 = mock(SyndEntry.class);
+        when(entry1.getPublishedDate()).thenReturn(earliest);
+        final SyndEntry entry2 = mock(SyndEntry.class);
+        when(entry2.getPublishedDate()).thenReturn(middle);
+        when(mockFeed.getEntries()).thenReturn(Arrays.asList(entry1, entry2));
+
+        final NewsFeed cut = new NewsFeed(mockFeed);
+        assertEquals(middle, cut.getPublishedDate());
     }
 }
